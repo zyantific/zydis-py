21efffd878ea12e4bc126fb2664d0dc3dac52e7e
Joel Höner <athre0z@zyantific.com>
2019-04-02 14:16:28 +0200

    Add project structure skeleton

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from glob import glob
+from distutils.command.build_clib import build_clib
+from Cython.Build import cythonize
+
+ZYDIS_C = ('zydis', {
+    'include_dirs': [
+        'zydis-c/include',
+        'zydis-c/src',
+        'zydis-c/dependencies/zycore/include',
+        'zydis-c/dependencies/zycore/src',
+        'zydis',
+    ],
+    'sources': glob('zydis-c/src/*.c') + glob('zydis-c/dependencies/src/*.c'),
+})
+
+setup(
+    name='zydis-py',
+    version='3.0.0',
+    packages=['zydis'],
+    libraries=[ZYDIS_C],
+    install_requires=['cython'],
+    url='https://zydis.re',
+    license='MIT',
+    author='Joel Höner',
+    author_email='athre0z@zyantific.com',
+    description='Python bindings for the fast & lightweight Zydis disassembler',
+    cmdclass={'build_clib': build_clib},
+    ext_modules=cythonize('zydis/ffi.pyx', language=3),
+)
