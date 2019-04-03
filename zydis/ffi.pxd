70a578c5190b33f8f9ab43682daf76ae35f5055e
Joel Höner <athre0z@zyantific.com>
2019-04-03 16:32:44 +0200

    Added enums and more features

--- zydis/ffi.pxd
+++ zydis/ffi.pxd
@@ -4,6 +4,7 @@
 from libc.stdint cimport (
     int16_t, int32_t, int64_t, int8_t,
     uint16_t, uint32_t, uint64_t, uint8_t,
+    uintptr_t, intptr_t,
 )
 
 # =========================================================================== #
@@ -15,12 +16,14 @@ ctypedef uint16_t  ZyanU16
 ctypedef uint32_t  ZyanU32
 ctypedef uint64_t  ZyanU64
 ctypedef size_t    ZyanUSize
+ctypedef uintptr_t ZyanUPointer
 
 ctypedef int8_t    ZyanI8
 ctypedef int16_t   ZyanI16
 ctypedef int32_t   ZyanI32
 ctypedef int64_t   ZyanI64
 ctypedef ptrdiff_t ZyanISize
+ctypedef intptr_t  ZyanIPointer
 
 ctypedef ZyanU8    ZyanBool
 
@@ -88,20 +91,28 @@ cdef extern from "Zydis/Zydis.h":
         pass  # Opaque.
 
     ctypedef enum ZydisDecoderMode:
-        pass  # TODO
+        ZYDIS_DECODER_MODE_MINIMAL
+        ZYDIS_DECODER_MODE_AMD_BRANCHES
+        ZYDIS_DECODER_MODE_KNC
+        ZYDIS_DECODER_MODE_MPX
+        ZYDIS_DECODER_MODE_CET
+        ZYDIS_DECODER_MODE_LZCNT
+        ZYDIS_DECODER_MODE_TZCNT
+        ZYDIS_DECODER_MODE_WBNOINVD
+        ZYDIS_DECODER_MODE_CLDEMOTE
 
     ctypedef enum ZydisMachineMode:
-        ZYDIS_MACHINE_MODE_LONG_64,
-        ZYDIS_MACHINE_MODE_LONG_COMPAT_32,
-        ZYDIS_MACHINE_MODE_LONG_COMPAT_16,
-        ZYDIS_MACHINE_MODE_LEGACY_32,
-        ZYDIS_MACHINE_MODE_LEGACY_16,
-        ZYDIS_MACHINE_MODE_REAL_16,
+        ZYDIS_MACHINE_MODE_LONG_64
+        ZYDIS_MACHINE_MODE_LONG_COMPAT_32
+        ZYDIS_MACHINE_MODE_LONG_COMPAT_16
+        ZYDIS_MACHINE_MODE_LEGACY_32
+        ZYDIS_MACHINE_MODE_LEGACY_16
+        ZYDIS_MACHINE_MODE_REAL_16
 
     ctypedef enum ZydisAddressWidth:
-        ZYDIS_ADDRESS_WIDTH_16,
-        ZYDIS_ADDRESS_WIDTH_32,
-        ZYDIS_ADDRESS_WIDTH_64,
+        ZYDIS_ADDRESS_WIDTH_16
+        ZYDIS_ADDRESS_WIDTH_32
+        ZYDIS_ADDRESS_WIDTH_64
 
     ctypedef struct ZydisDecodedInstruction:
         # TODO
@@ -140,12 +151,82 @@ cdef extern from "Zydis/Zydis.h":
 # --------------------------------------------------------------------------- #
 
 cdef extern from "Zydis/Zydis.h":
-    cpdef enum ZydisFormatterStyle_:
+    ctypedef enum ZydisFormatterStyle:
         ZYDIS_FORMATTER_STYLE_ATT
         ZYDIS_FORMATTER_STYLE_INTEL
         ZYDIS_FORMATTER_STYLE_INTEL_MASM
 
-    ctypedef ZydisFormatterStyle_ ZydisFormatterStyle
+    ctypedef enum ZydisFormatterProperty:
+        ZYDIS_FORMATTER_PROP_FORCE_SIZE
+        ZYDIS_FORMATTER_PROP_FORCE_SEGMENT
+        ZYDIS_FORMATTER_PROP_FORCE_RELATIVE_BRANCHES
+        ZYDIS_FORMATTER_PROP_FORCE_RELATIVE_RIPREL
+        ZYDIS_FORMATTER_PROP_PRINT_BRANCH_SIZE
+        ZYDIS_FORMATTER_PROP_DETAILED_PREFIXES
+        ZYDIS_FORMATTER_PROP_ADDR_BASE
+        ZYDIS_FORMATTER_PROP_ADDR_SIGNEDNESS
+        ZYDIS_FORMATTER_PROP_ADDR_PADDING_ABSOLUTE
+        ZYDIS_FORMATTER_PROP_ADDR_PADDING_RELATIVE
+        ZYDIS_FORMATTER_PROP_DISP_BASE
+        ZYDIS_FORMATTER_PROP_DISP_SIGNEDNESS
+        ZYDIS_FORMATTER_PROP_DISP_PADDING
+        ZYDIS_FORMATTER_PROP_IMM_BASE
+        ZYDIS_FORMATTER_PROP_IMM_SIGNEDNESS
+        ZYDIS_FORMATTER_PROP_IMM_PADDING
+        ZYDIS_FORMATTER_PROP_UPPERCASE_PREFIXES
+        ZYDIS_FORMATTER_PROP_UPPERCASE_MNEMONIC
+        ZYDIS_FORMATTER_PROP_UPPERCASE_REGISTERS
+        ZYDIS_FORMATTER_PROP_UPPERCASE_TYPECASTS
+        ZYDIS_FORMATTER_PROP_UPPERCASE_DECORATORS
+        ZYDIS_FORMATTER_PROP_DEC_PREFIX
+        ZYDIS_FORMATTER_PROP_DEC_SUFFIX
+        ZYDIS_FORMATTER_PROP_HEX_UPPERCASE
+        ZYDIS_FORMATTER_PROP_HEX_PREFIX
+        ZYDIS_FORMATTER_PROP_HEX_SUFFIX
+
+    ctypedef enum ZydisNumericBase:
+        ZYDIS_NUMERIC_BASE_DEC
+        ZYDIS_NUMERIC_BASE_HEX
+
+    ctypedef enum ZydisSignedness:
+        ZYDIS_SIGNEDNESS_AUTO
+        ZYDIS_SIGNEDNESS_SIGNED
+        ZYDIS_SIGNEDNESS_UNSIGNED
+
+    ctypedef enum ZydisPadding:
+        ZYDIS_PADDING_DISABLED
+        ZYDIS_PADDING_AUTO
+
+    ctypedef enum ZydisFormatterFunction:
+        ZYDIS_FORMATTER_FUNC_PRE_INSTRUCTION
+        ZYDIS_FORMATTER_FUNC_POST_INSTRUCTION
+        ZYDIS_FORMATTER_FUNC_FORMAT_INSTRUCTION
+        ZYDIS_FORMATTER_FUNC_PRE_OPERAND
+        ZYDIS_FORMATTER_FUNC_POST_OPERAND
+        ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_REG
+        ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_MEM
+        ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_PTR
+        ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_IMM
+        ZYDIS_FORMATTER_FUNC_PRINT_MNEMONIC
+        ZYDIS_FORMATTER_FUNC_PRINT_REGISTER
+        ZYDIS_FORMATTER_FUNC_PRINT_ADDRESS_ABS
+        ZYDIS_FORMATTER_FUNC_PRINT_ADDRESS_REL
+        ZYDIS_FORMATTER_FUNC_PRINT_DISP
+        ZYDIS_FORMATTER_FUNC_PRINT_IMM
+        ZYDIS_FORMATTER_FUNC_PRINT_TYPECAST
+        ZYDIS_FORMATTER_FUNC_PRINT_SEGMENT
+        ZYDIS_FORMATTER_FUNC_PRINT_PREFIXES
+        ZYDIS_FORMATTER_FUNC_PRINT_DECORATOR
+
+    ctypedef enum ZydisDecorator:
+        ZYDIS_DECORATOR_INVALID
+        ZYDIS_DECORATOR_MASK
+        ZYDIS_DECORATOR_BC
+        ZYDIS_DECORATOR_RC
+        ZYDIS_DECORATOR_SAE
+        ZYDIS_DECORATOR_SWIZZLE
+        ZYDIS_DECORATOR_CONVERSION
+        ZYDIS_DECORATOR_EH
 
     ctypedef struct ZydisFormatter:
         pass  # Opaque.
@@ -158,6 +239,18 @@ cdef extern from "Zydis/Zydis.h":
     ZyanStatus ZydisFormatterInit(
         ZydisFormatter* formatter,
         ZydisFormatterStyle style,
+    )
+
+    ZyanStatus ZydisFormatterSetProperty(
+        ZydisFormatter* formatter,
+        ZydisFormatterProperty property,
+        ZyanUPointer value,
+    )
+
+    ZyanStatus ZydisFormatterSetHook(
+        ZydisFormatter* formatter,
+        ZydisFormatterFunction type,
+        const void** callback,
     )
 
     ZyanStatus ZydisFormatterFormatInstruction(
--- zydis/zydis.pyx
+++ zydis/zydis.pyx
@@ -1,6 +1,8 @@
 # distutils: language=3
 # distutils: include_dirs=ZYDIS_INCLUDES
 
+import cython
+from pathlib import Path
 from ffi cimport *
 from enum import IntEnum
 
@@ -101,6 +103,34 @@ cdef inline int raise_if_err(ZyanStatus status) except -1:
 # [Decoder]                                                                   #
 # =========================================================================== #
 
+class DecoderMode(IntEnum):
+    MINIMAL = ZYDIS_DECODER_MODE_MINIMAL
+    AMD_BRANCHES = ZYDIS_DECODER_MODE_AMD_BRANCHES
+    KNC = ZYDIS_DECODER_MODE_KNC
+    MPX = ZYDIS_DECODER_MODE_MPX
+    CET = ZYDIS_DECODER_MODE_CET
+    LZCNT = ZYDIS_DECODER_MODE_LZCNT
+    TZCNT = ZYDIS_DECODER_MODE_TZCNT
+    WBNOINVD = ZYDIS_DECODER_MODE_WBNOINVD
+    CLDEMOTE = ZYDIS_DECODER_MODE_CLDEMOTE
+
+
+class MachineMode(IntEnum):
+    LONG_64 = ZYDIS_MACHINE_MODE_LONG_64
+    LONG_COMPAT_32 = ZYDIS_MACHINE_MODE_LONG_COMPAT_32
+    LONG_COMPAT_16 = ZYDIS_MACHINE_MODE_LONG_COMPAT_16
+    LEGACY_32 = ZYDIS_MACHINE_MODE_LEGACY_32
+    LEGACY_16 = ZYDIS_MACHINE_MODE_LEGACY_16
+    REAL_16 = ZYDIS_MACHINE_MODE_REAL_16
+
+
+class AddressWidth(IntEnum):
+    B16 = ZYDIS_ADDRESS_WIDTH_16
+    B32 = ZYDIS_ADDRESS_WIDTH_32
+    B64 = ZYDIS_ADDRESS_WIDTH_64
+
+
+@cython.freelist(16)
 cdef class DecodedInstruction:
     cdef ZydisDecodedInstruction instr
 
@@ -114,16 +144,21 @@ cdef class Decoder:
 
     def __cinit__(
         self,
-        ZydisMachineMode machine_mode = ZYDIS_MACHINE_MODE_LONG_64,
-        ZydisAddressWidth address_width = ZYDIS_ADDRESS_WIDTH_64,
+        machine_mode = MachineMode.LONG_64,
+        address_width = AddressWidth.B64,
     ):
         raise_if_err(ZydisDecoderInit(
             &self.decoder,
-            machine_mode,
-            address_width,
+            machine_mode.value,
+            address_width.value,
         ))
 
-    cpdef DecodedInstruction decode_one(self, bytes data):
+    cpdef void enable_mode(self, mode, ZyanBool enabled):
+        raise_if_err(ZydisDecoderEnableMode(
+            &self.decoder, mode.value, enabled
+        ))
+
+    cpdef DecodedInstruction decode(self, bytes data):
         cdef const unsigned char* data_ptr = data
         cdef DecodedInstruction instr = DecodedInstruction()
         raise_if_err(ZydisDecoderDecodeBuffer(
@@ -131,31 +166,99 @@ cdef class Decoder:
         ))
         return instr
 
-    def decode_all(self, bytes data):
-        while len(data) != 0:
-            insn = self.decode_one(data)
-            data = data[insn.length:]
-            yield insn
-
-    def decode_and_format_all(
-        self,
-        bytes data,
-        Formatter formatter = Formatter(),
-    ):
-        for insn in self.decode_all(data):
-            yield insn, formatter.format(insn)
 
 # =========================================================================== #
-# [Decoder]                                                                   #
+# [Formatter]                                                                 #
 # =========================================================================== #
+
+class FormatterStyle(IntEnum):
+    ATT = ZYDIS_FORMATTER_STYLE_ATT
+    INTEL = ZYDIS_FORMATTER_STYLE_INTEL
+    INTEL_MASM = ZYDIS_FORMATTER_STYLE_INTEL_MASM
+
+
+class FormatterProperty(IntEnum):
+    FORCE_SIZE = ZYDIS_FORMATTER_PROP_FORCE_SIZE
+    FORCE_SEGMENT = ZYDIS_FORMATTER_PROP_FORCE_SEGMENT
+    FORCE_RELATIVE_BRANCHES = ZYDIS_FORMATTER_PROP_FORCE_RELATIVE_BRANCHES
+    FORCE_RELATIVE_RIPREL = ZYDIS_FORMATTER_PROP_FORCE_RELATIVE_RIPREL
+    PRINT_BRANCH_SIZE = ZYDIS_FORMATTER_PROP_PRINT_BRANCH_SIZE
+    DETAILED_PREFIXES = ZYDIS_FORMATTER_PROP_DETAILED_PREFIXES
+    ADDR_BASE = ZYDIS_FORMATTER_PROP_ADDR_BASE
+    ADDR_SIGNEDNESS = ZYDIS_FORMATTER_PROP_ADDR_SIGNEDNESS
+    ADDR_PADDING_ABSOLUTE = ZYDIS_FORMATTER_PROP_ADDR_PADDING_ABSOLUTE
+    ADDR_PADDING_RELATIVE = ZYDIS_FORMATTER_PROP_ADDR_PADDING_RELATIVE
+    DISP_BASE = ZYDIS_FORMATTER_PROP_DISP_BASE
+    DISP_SIGNEDNESS = ZYDIS_FORMATTER_PROP_DISP_SIGNEDNESS
+    DISP_PADDING = ZYDIS_FORMATTER_PROP_DISP_PADDING
+    IMM_BASE = ZYDIS_FORMATTER_PROP_IMM_BASE
+    IMM_SIGNEDNESS = ZYDIS_FORMATTER_PROP_IMM_SIGNEDNESS
+    IMM_PADDING = ZYDIS_FORMATTER_PROP_IMM_PADDING
+    UPPERCASE_PREFIXES = ZYDIS_FORMATTER_PROP_UPPERCASE_PREFIXES
+    UPPERCASE_MNEMONIC = ZYDIS_FORMATTER_PROP_UPPERCASE_MNEMONIC
+    UPPERCASE_REGISTERS = ZYDIS_FORMATTER_PROP_UPPERCASE_REGISTERS
+    UPPERCASE_TYPECASTS = ZYDIS_FORMATTER_PROP_UPPERCASE_TYPECASTS
+    UPPERCASE_DECORATORS = ZYDIS_FORMATTER_PROP_UPPERCASE_DECORATORS
+    DEC_PREFIX = ZYDIS_FORMATTER_PROP_DEC_PREFIX
+    DEC_SUFFIX = ZYDIS_FORMATTER_PROP_DEC_SUFFIX
+    HEX_UPPERCASE = ZYDIS_FORMATTER_PROP_HEX_UPPERCASE
+    HEX_PREFIX = ZYDIS_FORMATTER_PROP_HEX_PREFIX
+    HEX_SUFFIX = ZYDIS_FORMATTER_PROP_HEX_SUFFIX
+
+
+class NumericBase(IntEnum):
+    DEC = ZYDIS_NUMERIC_BASE_DEC
+    HEX = ZYDIS_NUMERIC_BASE_HEX
+
+
+class Signedness(IntEnum):
+    AUTO = ZYDIS_SIGNEDNESS_AUTO
+    SIGNED = ZYDIS_SIGNEDNESS_SIGNED
+    UNSIGNED = ZYDIS_SIGNEDNESS_UNSIGNED
+
+
+class Padding(IntEnum):
+    DISABLED = ZYDIS_PADDING_DISABLED
+    AUTO = ZYDIS_PADDING_AUTO
+
+
+class FormatterFunction(IntEnum):
+    PRE_INSTRUCTION = ZYDIS_FORMATTER_FUNC_PRE_INSTRUCTION
+    POST_INSTRUCTION = ZYDIS_FORMATTER_FUNC_POST_INSTRUCTION
+    FORMAT_INSTRUCTION = ZYDIS_FORMATTER_FUNC_FORMAT_INSTRUCTION
+    PRE_OPERAND = ZYDIS_FORMATTER_FUNC_PRE_OPERAND
+    POST_OPERAND = ZYDIS_FORMATTER_FUNC_POST_OPERAND
+    FORMAT_OPERAND_REG = ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_REG
+    FORMAT_OPERAND_MEM = ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_MEM
+    FORMAT_OPERAND_PTR = ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_PTR
+    FORMAT_OPERAND_IMM = ZYDIS_FORMATTER_FUNC_FORMAT_OPERAND_IMM
+    PRINT_MNEMONIC = ZYDIS_FORMATTER_FUNC_PRINT_MNEMONIC
+    PRINT_REGISTER = ZYDIS_FORMATTER_FUNC_PRINT_REGISTER
+    PRINT_ADDRESS_ABS = ZYDIS_FORMATTER_FUNC_PRINT_ADDRESS_ABS
+    PRINT_ADDRESS_REL = ZYDIS_FORMATTER_FUNC_PRINT_ADDRESS_REL
+    PRINT_DISP = ZYDIS_FORMATTER_FUNC_PRINT_DISP
+    PRINT_IMM = ZYDIS_FORMATTER_FUNC_PRINT_IMM
+    PRINT_TYPECAST = ZYDIS_FORMATTER_FUNC_PRINT_TYPECAST
+    PRINT_SEGMENT = ZYDIS_FORMATTER_FUNC_PRINT_SEGMENT
+    PRINT_PREFIXES = ZYDIS_FORMATTER_FUNC_PRINT_PREFIXES
+    PRINT_DECORATOR = ZYDIS_FORMATTER_FUNC_PRINT_DECORATOR
+
+
+class Decorator(IntEnum):
+    INVALID = ZYDIS_DECORATOR_INVALID
+    MASK = ZYDIS_DECORATOR_MASK
+    BC = ZYDIS_DECORATOR_BC
+    RC = ZYDIS_DECORATOR_RC
+    SAE = ZYDIS_DECORATOR_SAE
+    SWIZZLE = ZYDIS_DECORATOR_SWIZZLE
+    CONVERSION = ZYDIS_DECORATOR_CONVERSION
+    EH = ZYDIS_DECORATOR_EH
+
 
 cdef class Formatter:
     cdef ZydisFormatter formatter
 
-    def __cinit__(
-        self,
-        ZydisFormatterStyle style = ZYDIS_FORMATTER_STYLE_INTEL,
-    ):
+    def __cinit__(self, style = FormatterStyle.INTEL):
         raise_if_err(ZydisFormatterInit(&self.formatter, style))
 
     cpdef str format(self, DecodedInstruction instr):
@@ -164,5 +267,72 @@ cdef class Formatter:
             &self.formatter, &instr.instr, buffer, sizeof(buffer), 0
         ))
         return buffer.decode('utf8')
+
+# =========================================================================== #
+# [Convenience / helpers]                                                     #
+# =========================================================================== #
+
+cdef Decoder STATIC_DECODER = Decoder()
+cdef Formatter STATIC_FORMATTER = Formatter()
+
+
+def decode_all(bytes data, *, Decoder decoder = STATIC_DECODER):
+    """
+    Generator lazily decoding all instructions in the given bytes object,
+    yielding `DecodedInstruction` instances. A decoder can be explicitly
+    specified via the `decoder` argument. If omitted, a shared decoder
+    (with default settings) is used.
+    """
+    while len(data) != 0:
+        instr = decoder.decode(data)
+        data = data[instr.length:]
+        yield instr
+
+
+def decode_and_format_all(
+    bytes data,
+    *,
+    Decoder decoder = STATIC_DECODER,
+    Formatter formatter = STATIC_FORMATTER,
+):
+    """
+    Generator lazily decoding and formatting all instructions in the given
+    bytes object, yielding `(DecodedInstruction, str)` pairs. `Decoder`
+    and `Formatter` can be explicitly specified via the `decoder` /
+    `formatter` arguments. If omitted, a shared decoder / formatter
+    (with default settings) is used.
+    """
+    for instr in decode_all(data, decoder=decoder):
+        yield instr, formatter.format(instr)
+
+
+# =========================================================================== #
+# [Auto generated enums]                                                      #
+# =========================================================================== #
+
+cdef make_enum(name, prefix):
+    header_dir = '../zydis-c/include/Zydis/Generated'
+    file = f'Enum{name}.h'
+    path = Path(__file__).parent / header_dir / file
+
+    with path.open('r') as f:
+        lines = [x.strip() for x in f]
+
+    prefix_len = len(prefix)
+    enum_vals = [
+        line[prefix_len:-1]  # -1 cuts away the ,
+        for line in lines
+        if line.startswith(prefix)
+    ]
+
+    assert enum_vals, enum_vals
+    return IntEnum(name, enum_vals)
+
+
+Mnemonic = make_enum('Mnemonic', 'ZYDIS_MNEMONIC_')
+ISAExt = make_enum('ISAExt', 'ZYDIS_ISA_EXT_')
+ISASet = make_enum('ISASet', 'ZYDIS_ISA_SET_')
+InstructionCategory = make_enum('InstructionCategory', 'ZYDIS_CATEGORY_',)
+Register = make_enum('Register', 'ZYDIS_REGISTER_')
 
 # =========================================================================== #
