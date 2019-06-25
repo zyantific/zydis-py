eb0a4695f88c2988a2cb0a4b5c0be122ce2cdca6
Joel Höner <athre0z@zyantific.com>
2019-06-25 21:23:22 +0200

    Support build without Cython

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+import setuptools
 from glob import glob
 from distutils.command.build_clib import build_clib
-from Cython.Build import cythonize
+from distutils.core import setup
+from distutils.extension import Extension
+
+CYTHON_MODULES = glob('zydis/*.pyx')
 
 ZYDIS_INCLUDE_DIRS = [
     './zydis-c/include',
     './zydis-c/src',
     './zydis-c/dependencies/zycore/include',
     './zydis-c/dependencies/zycore/src',
-    './zydis',
+    './cfgheaders',
 ]
 
 ZYDIS_C = ('zydis', {
@@ -18,22 +21,43 @@ ZYDIS_C = ('zydis', {
     'sources': glob('zydis-c/src/*.c') + glob('zydis-c/dependencies/src/*.c'),
 })
 
+
+try:
+    from Cython.Build import cythonize
+    def maybe_cythonize(ext):
+        cythonize(
+            ext,
+            aliases={'ZYDIS_INCLUDES': ZYDIS_INCLUDE_DIRS},
+            # build_dir='build',
+            language_level=3,
+        )
+except ImportError:
+    def maybe_cythonize(_):
+        pass
+
+
+for module in CYTHON_MODULES:
+    maybe_cythonize(module)
+
+
 setup(
     name='zydis-py',
     version='3.0.0a0',
     packages=['zydis'],
     libraries=[ZYDIS_C],
-    install_requires=['Cython<=0.30'],
+    # install_requires=['Cython<=0.30'],
     url='https://zydis.re',
     license='MIT',
     author='Joel Höner',
     author_email='athre0z@zyantific.com',
     description='Python bindings for the fast & lightweight Zydis disassembler',
     cmdclass={'build_clib': build_clib},
-    ext_modules=cythonize(
-        glob('zydis/*.pyx'),
-        aliases={'ZYDIS_INCLUDES': ZYDIS_INCLUDE_DIRS},
-        build_dir='build',
-        language_level=3,
-    ),
+    ext_modules=[
+        Extension(
+            name=mod.split('.')[0].replace('/', '.'),
+            sources=[mod.split('.')[0] + '.c'],
+            include_dirs=ZYDIS_INCLUDE_DIRS,
+        )
+        for mod in CYTHON_MODULES
+    ],
 )
